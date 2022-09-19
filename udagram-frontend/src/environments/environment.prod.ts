aad552851a8aab8095082dc3fbc495b123954aa0
Reda <muhammadredabh@gmail.com>
2022-09-19 23:09:44 +0100

    added deployment file

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a51ba159bce26481b8e71846daeaf774-1527428930.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
